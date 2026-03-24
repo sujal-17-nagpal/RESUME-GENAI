fa4c8f1a6bebc8ffdf9c2293835cd6a799157e98
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-03-24 21:17:26 +0530

    interview report model added

---
+++ backend/models/InterviewReport.model.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+
+const technicalQuestionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: [true, "Question is required"],
+    },
+    intention: {
+      type: String,
+      required: [true, "Intention is required"],
+    },
+    answer: {
+      type: String,
+      required: [true, "Answer is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const behavioralQuestionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: [true, "Question is required"],
+    },
+    intention: {
+      type: String,
+      required: [true, "Intention is required"],
+    },
+    answer: {
+      type: String,
+      required: [true, "Answer is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const skillGapSchema = new mongoose.Schema(
+  {
+    skill: {
+      type: String,
+      required: [true, "Skill is required"],
+    },
+    severinity: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      required: [true, "Severinity is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const preparationPlanSchema = new mongoose.Schema(
+  {
+    day: {
+      type: Number,
+      required: [true, "Day is required"],
+    },
+    focus: {
+      type: String,
+      required: [true, "Focus is required"],
+    },
+    tasks: {
+      type: [String],
+      required: [true, "Tasks are required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const interviewReportSchema = new mongoose.Schema({
+  jobDesciption: {
+    type: String,
+    required: [true, "Job description is required"],
+  },
+  resume: {
+    type: String,
+  },
+  selfDescription: {
+    type: String,
+  },
+  matchScore: {
+    type: Number,
+    min: 0,
+    max: 100,
+  },
+});
