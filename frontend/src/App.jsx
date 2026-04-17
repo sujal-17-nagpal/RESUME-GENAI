68786db1db9162ec0698c2487519b173f9830063
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-04-17 23:00:54 +0530

    created interview provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app.routes";
 import { AuthProvider } from "./features/auth/auth.context";
+import { InterviewProvider } from "./features/interview/interview.context";
 
 function App() {
   return (
     <AuthProvider>
+      <InterviewProvider>
       <RouterProvider router={router} />
+
+      </InterviewProvider>
     </AuthProvider>
   );
 }
