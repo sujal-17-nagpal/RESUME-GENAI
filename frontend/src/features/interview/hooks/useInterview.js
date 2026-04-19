067a5d1695b67aaa7aa8fb29940ef6d8f3f75b3e
sujal-17-nagpal <sujaln0609y@gmail.com>
2026-04-20 01:13:28 +0530

    created custom hook

---
+++ frontend/src/features/interview/hooks/useInterview.js
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import { InterviewContext } from "../interview.context";
+import { generateInterviewReportApi, getAllInterviewReports, getInterviewReportById } from "../services/interview.api";
+
+export const useInterview = ()=>{
+    const context = useContext(InterviewContext)
+
+    if(!context){
+        throw new Error("useInterview must be in Interview context");
+    }
+
+    const { loading, setLoading, report, setReport, reports, setReports } = context
+
+    const generateReport = async({jobDescription,selfDescription,resumeFile})=>{
+        setLoading(true)
+        try {
+            const response = await generateInterviewReportApi({jobDescription,selfDescription,resumeFile})
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.log(error)
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    const getReportById = async(interviewId)=>{
+        setLoading(true)
+        try {
+            const response = await  getInterviewReportById(interviewId)
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.log(error)
+        } finally{
+            setLoading(false)
+        }
+    }
+
+    const getReports = async()=>{
+        setLoading(false)
+        try {
+            const response = await getAllInterviewReports()
+        setReports(response.interviewReports)
+    
+        } catch (error) {
+            console.log(error)
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    return {loading,report,reports,generateReport,getReportById,getReports}
+}
